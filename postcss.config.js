984ac59a33731295765612355c83bf2c86ca3f13
markvayson <markvayson@gmail.com>
2023-07-31 22:45:09 +0400

    added plugins for webpack: tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    [
+      "postcss-preset-env",
+      {
+        taildwindcss: {},
+        autoprefixer: {},
+      },
+    ],
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
